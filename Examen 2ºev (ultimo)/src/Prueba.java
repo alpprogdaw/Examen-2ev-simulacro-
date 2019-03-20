3901254928985a8f510bef27bc5c5b0cc0040696
adrian <alodeiroperezrgmail.com>
2019-03-20 18:27:03 +0100

    ewe

--- Examen 2ºev (ultimo)/src/Prueba.java
+++ Examen 2ºev (ultimo)/src/Prueba.java
@@ -1,11 +1,35 @@
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
 
 public class Prueba {
 
-	public static void main(String[] args) {
-		Electrodomesticos lavadora = new Lavadora();
-		ArrayList<Electrodomesticos> lista = new ArrayList<>(); 
-
+	public static void main(String[] args) throws Exception {
+		List<Electrodomesticos> lista = new ArrayList<>(); 
+		lista.add(new Televisores(573.99f, Color.NEGRO, Consumo.A, 2.5f, 40, TDT.normal));
+		lista.add(new Lavadora(300f, Color.GRIS, Consumo.B, 63f, 5));
+		lista.add(new Frigorificos(659.99f, Color.BLANCO, Consumo.B, 47, false));
+		mostrarLista(lista);
+		Collections.sort(lista);
+		mostrarLista(lista);
+		Collections.sort(lista, Collections.reverseOrder());
+		mostrarLista(lista);
+		Collections.sort(lista, new ComparatorPeso());
+		mostrarLista(lista);
+		Collections.sort(lista, Collections.reverseOrder(new ComparatorPeso()));
+		mostrarLista(lista);
+		Collections.sort(lista, new ComparatorConsumoColor());
+		mostrarLista(lista);
+		mostrarLista(lista);
+		Collections.sort(lista);
+		mostrarLista(lista);
 	}
-
+	
+	public static void mostrarLista(List<Electrodomesticos> lista) {
+		Iterator<Electrodomesticos> it = lista.iterator();
+		while (it.hasNext()) {
+			System.out.println(it.next());
+		}
+	}
 }
