e599cadb37f657ad6ceae67bc9aca6812aa1fa85
adrian <alodeiroperezrgmail.com>
2019-03-20 18:07:07 +0100

    AC

--- Examen 2ºev (ultimo)/src/Lavadora.java
+++ Examen 2ºev (ultimo)/src/Lavadora.java
@@ -2,57 +2,33 @@
 public class Lavadora extends Electrodomesticos {
 	private int carga = 5;
 
-	public Lavadora(float precio_base, Color color, Consumo consumo, float peso, int carga) {
+	public Lavadora(float precio_base, Color color, Consumo consumo, float peso, int carga) throws Exception {
 		super(precio_base, color, consumo, peso);
-		this.carga = carga;
+		setCarga(carga);
 	}
 	
 	public Lavadora(float precio_base, float peso, int carga) throws Exception {
 		super(precio_base, peso);
-		this.carga = carga;
+		setCarga(carga);
+	}
+	
+	@Override
+	public String toString() {
+		return super.toString() + "Lavadora [carga=" + carga + "]";
 	}
 
-	public int precioFinal(int precio_base) {
-		int precio_consumo = 0;
-		int precio_peso = 0;
-		int precio_carga = 0;
-		int precio_final;
-		switch (consumo) {
-		case A:
-			precio_consumo = precio_base + 100;
-			break;
-		case B:
-			precio_consumo = precio_base + 80;
-			break;
-		case C:
-			precio_consumo = precio_base + 60;
-			break;
-		case D:
-			precio_consumo = precio_base + 50;
-			break;
-		case E:
-			precio_consumo = precio_base + 30;
-			break;
-		case F:
-			precio_consumo = precio_base + 10;
-			break;
-		}
-		if (peso > 0 && peso < 19) {
-			precio_peso = precio_base + 10;
-		}
-		if (peso > 20 && peso < 49) {
-			precio_peso = precio_base + 50;
-		}
-		if (peso > 50 && peso < 79) {
-			precio_peso = precio_base + 80;
-		}
-		if (peso > 80) {
-			precio_peso = precio_base + 100;
-		}
+	private void setCarga(int carga) throws Exception {
+		if (carga != 4 && carga != 5 && carga != 6 && carga != 7
+				 && carga != 8 && carga != 10 && carga != 11 && carga != 13)
+			throw new Exception("Error: carga especificada: "  + carga + ". La carga tiene que ser 4, 5, 6, 7, 8, 10, 11 o 13");
+		this.carga = carga;  //lo indicamos aqui para no tener que ponerlo arriba en los constructores
+	}
+
+	public float precioFinal() {
+		float precio_final = super.precioFinal();
 		if (carga > 8) {
-			precio_carga = precio_base + 50;
+			precio_final = precio_final +  50;
 		}
-		precio_final = precio_base + (precio_consumo + precio_peso + precio_carga);
 		return precio_final;
 	}
 }
--- Examen 2ºev (ultimo)/src/Televisores.java
+++ Examen 2ºev (ultimo)/src/Televisores.java
@@ -3,49 +3,16 @@ public class Televisores extends Electrodomesticos {
 	private float tamaño = 20;
 	private TDT tdt = TDT.normal;
 	
-	public Televisores(float tamaño, TDT tdt) {
+	public Televisores(float precio_base, float peso, float tamaño, TDT tdt) throws Exception {
+		super(precio_base, peso);
+		this.tamaño = tamaño;
+		this.tdt = tdt;
+	}
+
+	public Televisores(float precio_base, Color color, Consumo consumo, float peso, float tamaño, TDT tdt) {
 		super(precio_base, color, consumo, peso);
 		this.tamaño = tamaño;
 		this.tdt = tdt;
 	}
 
-	public  int precioFinal(int precio_base) {
-		int precio_consumo = 0;
-		int precio_peso = 0;
-		int precio_final;
-		switch (consumo) {
-		case A:
-			precio_consumo = precio_base + 100;
-			break;
-		case B:
-			precio_consumo = precio_base + 80;
-			break;
-		case C:
-			precio_consumo = precio_base + 60;
-			break;
-		case D:
-			precio_consumo = precio_base + 50;
-			break;
-		case E:
-			precio_consumo = precio_base + 30;
-			break;
-		case F:
-			precio_consumo = precio_base + 10;
-			break;
-		}
-		if (peso > 0 && peso < 19) {
-			precio_peso = precio_base + 10;
-		}
-		if (peso > 20 && peso < 49) {
-			precio_peso = precio_base + 50;
-		}
-		if (peso > 50 && peso < 79) {
-			precio_peso = precio_base + 80;
-		}
-		if (peso > 80) {
-			precio_peso = precio_base + 100;
-		}
-		precio_final = precio_base +  (precio_consumo + precio_peso);
-		return precio_final;
-	}
 }
